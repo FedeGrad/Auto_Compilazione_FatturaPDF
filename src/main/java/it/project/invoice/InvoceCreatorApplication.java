9588b4aeac4ebf5cf44ebc50710862d3cb78945d
Federico Gradizzi <101737604+FedeGrad@users.noreply.github.com>
2022-06-02 11:26:30 +0200

    Condivisione progetto

---
+++ src/main/java/it/project/invoice/InvoceCreatorApplication.java
@@ -0,0 +1,13 @@
+package it.project.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoceCreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoceCreatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/project/invoice/InvoceCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.project.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoceCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
